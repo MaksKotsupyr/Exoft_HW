1dd22a491894917c60c9a77546be2244b78b6d3f
Maksym Kotsupyr <maksym.kotsupyr@gmail.com>
2021-10-12 18:53:30 +0300

    Completed HW1 and HW2, start HW3

---
+++ HW_2/HW_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace ExtensionMethod
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student {ID = 101, Name = "Preety" },
+                new Student {ID = 102, Name = "Sambit" },
+                new Student {ID = 103, Name = "Hina"},
+                new Student {ID = 104, Name = "Anurag"},
+                new Student {ID = 102, Name = "Sambit"},
+                new Student {ID = 103, Name = "Hina"},
+                new Student {ID = 101, Name = "Preety" },
+            };
+            return students;
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            var MS = Student.GetStudents()
+                   .Select(std => $"{ std.Name}-{ std.ID}")
+                   .Distinct().ToList();
+            foreach (var item in MS)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
+
---
+++ HW_3/HW3_Task/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW3_Task
+{
+    class Program
+    {
+        static void Main()
+        {
+            int countCalls = 1;
+            var callList = new List<Call>
+            {
+                new Call(4000, 2340),
+                new Call(8000, 2345),
+                new Call(4000, 2221),
+                new Call(4000, 2329),
+                new Call(4000, 9934),
+                new Call(4000, 5614),
+                new Call(4000, 9901),
+                new Call(4000, 1002),
+                new Call(4000, 3032)
+            };
+            foreach (var call in callList)
+            {
+                Console.WriteLine($"{countCalls++}-{call}");
+            }
+            while (true)
+            {
+                Console.WriteLine("To start call write 'call'| |To cancel call write 'cancel'| |To see call history 'log'");
+                string inputValue = Console.ReadLine();
+                if (inputValue == "call")
+                {
+                    Console.WriteLine("To start call write 'phonenumber'");
+                    string inputNumber = Console.ReadLine();
+                    //if(inputNumber == call.)
+                }
+                else if(inputValue == "cancel")
+                {
+                    Console.WriteLine("To start call write 'phonenumber'");
+                    string inputNumber = Console.ReadLine();
+                }
+                else if (inputValue == "log")
+                {
+                    foreach (var call in callList)
+                    {
+                        Console.WriteLine($"PhoneNumber: [{call.PhoneNumber}] {call.Status}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command");
+                }
+            }
+        }
+    }
+}
